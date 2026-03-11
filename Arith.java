332cd3eb341e7e560c2fb7b73ebcf9055c7585ba
Janhavi Mallya <janhavimallya@gmail.com>
2026-03-11 09:26:28 +0530

    Create Arith.java

---
+++ Arith.java
@@ -0,0 +1,12 @@
+class Arith
+  {
+    public static void main (String args[])
+    {
+      int a=12;
+      int b=32;
+      System.out.println("addition of 2 numbers"+(a+b));
+      System.out.println("difference of 2 numbers"+(a-b));
+      System.out.println("division of 2 numbers"+(a/b));
+      System.out.println("product of 2 numbers"+(a*b));
+    }
+  }
